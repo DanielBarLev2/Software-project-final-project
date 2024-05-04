c42a766d3bba2d56eb8440d8e2d1de166ea9d769
danielbarlev2 <danielbl2010@gmail.com>
2024-05-04 20:08:55 +0300

    test_similarity_matrix test3

--- tests/test_symnmf.py
+++ tests/test_symnmf.py
@@ -1,4 +1,4 @@
-from src.symnmf import create_similarity_matrix, read_data
+from src.symnmf import similarity_matrix, read_data
 import pytest
 import numpy as np
 
@@ -11,13 +11,18 @@ def test_read_data():
                                     [7, 8, 9]]), x)
 
 
-def test_create_similarity_matrix():
-    a1 = create_similarity_matrix(x=np.array([[1, 2, 3],
-                                              [4, 5, 6]]), n=2)
+def test_similarity_matrix():
+    a1 = similarity_matrix(X=np.array([[1, 2, 3],
+                                       [4, 5, 6]]), n=2)
 
-    a2 = create_similarity_matrix(x=np.array([[1, 2, 3],
-                                              [4, 5, 6],
-                                              [7, 8, 9]]), n=3)
+    a2 = similarity_matrix(X=np.array([[1, 2, 3],
+                                       [4, 5, 6],
+                                       [7, 8, 9]]), n=3)
+
+    a3 = similarity_matrix(X=np.array([[0.1, 0.2, 0.1],
+                                       [0.1, 0.1, 0.1]]), n=2)
+
+
 
     a1_correct = np.array([[0, pytest.approx(expected=1.36096e-6, abs=1e-6)],
                            [pytest.approx(expected=1.36096e-6, abs=1e-6), 0]])
@@ -36,14 +41,19 @@ def test_create_similarity_matrix():
 
     np.fill_diagonal(a2_correct, 0)
 
+    a3_correct = np.array([[0, pytest.approx(expected=0.995012, abs=1e-5)],
+                           [pytest.approx(expected=0.995012, abs=1e-5), 0]])
+
     assert np.array_equal(a1, a1_correct)
 
     assert np.array_equal(a2, a2_correct)
 
+    assert np.array_equal(a3, a3_correct)
+
 
 def main():
     test_read_data()
-    test_create_similarity_matrix()
+    test_similarity_matrix()
 
 
 if __name__ == '__main__':
